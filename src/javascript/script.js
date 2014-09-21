fd90bf7bbd09cf18e1ca3ad3f201f60a9dfb30b3
FireElementalNE <FireElemental.NE@gmail.com>
2014-09-21 13:16:11 -0400

    updated to allow for simple room switching

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,14 @@
 console.log("loaded");
 
+var player; 
+
 var counter = 0;
+$( document ).ready(function() {
+  player = {
+	"inventory": [],
+	"currentRoom":  world.rooms[0]
+	}
+});
 
 function writeTextToOutput(type,msg) {
 	var writerClass = undefined;
@@ -19,14 +27,65 @@ function writeTextToOutput(type,msg) {
 	$('#chatArea').append(el);
 }
 
+function chomp(str) {
+	return str.replace(/(\n|\r|\s)+$/, '');
+}
+
+function parseCommand(com) {
+	moveRe = /move\s(Room\d)/i;
+	adjRe = /list\sadjacent/i;
+	switch(true) {
+		case moveRe.test(com): 
+			move(moveRe.exec(com)[1]);
+			break;
+		case adjRe.test(com): 
+			console.log("adj");
+			break;
+	}
+}
+
+
 function submitUserCommand() {
 	var data = $("#chatMessege").val();
 	$("#chatMessege").val("");
 	writeTextToOutput('user',data);
 	$("#chatMessege").focus();
+	parseCommand(chomp(data.toLowerCase()));
 }
 
-setInterval(function(){
-      writeTextToOutput('console',counter);
+function findRoom(id) {
+	for(var i = 0; i < world.rooms.length; i++) {
+		if(world.rooms[i].id == id) {
+			return world.rooms[i];
+		}
+	}
+	return null;
+}
+
+function ListAdj(cRoom) {
+
+}
+
+function move(dRoom) {
+	var found = false;
+	actualAdjRooms = player.currentRoom.adjacentRooms;
+	for(var i = 0; i < actualAdjRooms.length; i++) {
+		console.log('#' + actualAdjRooms[i] + ' ' + dRoom + '#');
+		if(dRoom == actualAdjRooms[i]) {
+			found = true;
+			break;
+		}
+	}
+	if(found) {
+		var newRoom = findRoom(dRoom);
+		writeTextToOutput('console','Moving to \'' + dRoom + '\'.');
+		player.currentRoom = newRoom;
+	}
+	else {
+		writeTextToOutput('console','\'' + dRoom + '\' is not adjacent to \'' + player.currentRoom.id + '\'.');
+	}
+}
+
+setInterval(function(){      
       counter++;
 },500);
---
+++ src/javascript/world.js
@@ -0,0 +1,17 @@
+var world = {
+	"rooms":
+		[
+			{
+				id: "room1",
+				name: "Bedroom",
+				objects: ["a","b","c"],
+				adjacentRooms: ["room2"]
+			},
+			{
+				id: "room2",
+				name: "Lobby Cyber",
+				objects: ["a","b","c"],
+				adjacentRooms: ["room1"]
+			}
+		]
+}
